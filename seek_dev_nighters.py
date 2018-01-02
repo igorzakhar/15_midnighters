34c4f9f13fb62e4cff377e20fe7c7c592366d956
igorzakhar <igorzakhar@gmail.com>
2018-01-02 12:35:18 +0300

    Added print_output() func

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import time, datetime
 
 import requests
 import pytz
@@ -9,34 +9,42 @@ URL_API = 'https://devman.org/api/challenges/solution_attempts/'
 def get_number_of_pages():
     resp = requests.get(URL_API)
     resp_dict = resp.json()
-    page_count = resp_dict['number_of_pages']
-    return page_count
+    number_of_pages = resp_dict['number_of_pages']
+    return number_of_pages
 
 
 def load_attempts(number_of_pages):
-    for page in range(1, number_of_pages + 1):
-        query_str = '{}?page={}'.format(URL_API, page)
-        resp = requests.get(query_str)
+    for page_number in range(1, number_of_pages + 1):
+        payload = {'page': str(page_number)}
+        resp = requests.get(URL_API, params=payload)
         records = resp.json()['records']
         for record in records:
             yield record
 
 
-def get_midnighters(load_attempts):
+def get_midnighters(records):
+    start_time = time(0, 0)
+    end_time = time(6, 0)
     midnighters = set()
-    for item in load_attempts:
+    for item in records:
         time_zone = pytz.timezone(item['timezone'])
         local_datetime = datetime.fromtimestamp(item['timestamp'], time_zone)
-        if (5 >= local_datetime.hour >= 0):
+        if (end_time >= local_datetime.time() >= start_time):
             midnighters.add(item['username'])
     return midnighters
 
 
+def print_output(content):
+    print("Users, who have sent a solution after 00-00:")
+    for item in content:
+        print(item)
+
+
 def main():
     number_of_pages = get_number_of_pages()
-    attempts = load_attempts(number_of_pages)
-    midnighters = get_midnighters(attempts)
-    print(midnighters)
+    content = load_attempts(number_of_pages)
+    midnighters = get_midnighters(content)
+    print_output(midnighters)
 
 
 if __name__ == '__main__':
